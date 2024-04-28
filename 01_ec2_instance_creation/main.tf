8560db2c40cc31a79601919c07e5173f82eac605
Shivani Shah <shivanishahwork2@gmail.com>
2024-04-27 17:36:33 -0700

    My first terraform file

---
+++ 01_ec2_instance_creation/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "my-ec2-instance" {
+  ami                     = "ami-0827b6c5b977c020e"
+  instance_type           = "t2.micro"
+  key_name                = "dev-keypair"
+  
+  tags = {
+    Name = "test-1"
+  }
+}
